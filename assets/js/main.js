2fa089eb584207e2fc27f35dd0205432c6732c9b
dynat3k <dynat3k@gmail.com>
2025-04-01 22:13:00 -0400

    updates to logo and img

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,10 @@ Author: GrayGrids
         var logo = document.querySelector('.navbar-brand img')
         if (window.pageYOffset > sticky) {
           header_navbar.classList.add("sticky");
-          logo.src = 'assets/images/logo/logo.svg';
+          logo.src = 'assets/images/logo/stamp_d_logo_3.png';
         } else {
           header_navbar.classList.remove("sticky");
-          logo.src = 'assets/images/logo/white-logo.svg';
+          logo.src = 'assets/images/logo/stamp_d_logo (2).png';
         }
 
         // show or hide the back-top-top button
